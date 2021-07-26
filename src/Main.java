ec19c5011e46546dad74a9edfc420f962d060195
Ada W <adabingw@yahoo.com>
2021-07-26 13:49:31 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,65 @@ public class Main {
 		QuestionPane Q = new QuestionPane();
 		int NUnits = Q.AskUnits(); 
 		System.out.println("Number of Units: " + NUnits); // system check
+				int[] unitUNweighted = new int[NUnits];
 		int[] unitweighted = new int[NUnits]; 
-		double[] Weights = new double[NUnits];
+		int[] Weights = new int[NUnits];
+		
 		for (int i = 0; i < NUnits; i++) {
-			double Weight = Q.Unitw(i+1); 
+			int Weight = Q.Unitw(i+1); 
 			Weights[i] = Weight;
 			int NumberofAssignments = Q.AskAssign(); 
-			int AssignmentAvg = Q.InputAssign(NumberofAssignments);
-			unitweighted[i] = (int) ((AssignmentAvg + Q.InputUnitFinal()) * Weight);
-		}
+			int[] AssignmentArray = Q.InputAssign(NumberofAssignments);
+				int sumOfAssignments = IntStream.of(AssignmentArray).sum();
+				int averageAssignments = (int) Math.round((sumOfAssignments / NumberofAssignments) * 0.7);
+			int InputedUFinal = Q.InputUnitFinal();
+		    int UnitFinaleW = (int) Math.round(InputedUFinal * 0.3);
+			unitUNweighted[i] = (int) Math.round(averageAssignments + UnitFinaleW);
+			unitweighted[i] = (int) Math.round(unitUNweighted[i] * ((double) Weight/100));
+			
+			try {
+				Connection connection = DriverManager.getConnection(url, username, password);
+				String sql = "INSERT INTO unit (UNIT_NO, UNIT_W, UNIT_FINAL, UNIT_MARK) VALUES (?, ?, ?, ?)";
+				PreparedStatement statement = connection.prepareStatement(sql);
+				statement.setInt(1, i + 100);
+				statement.setInt(2, Weights[i]);
+				statement.setInt(3, InputedUFinal);
+				statement.setInt(4, unitUNweighted[i]);
+				
+				System.out.println("connection success!!");
+				
+				int rows = statement.executeUpdate();
+				if (rows > 0) {
+					System.out.println("A row has been inserted.");
+				}
+				
+				String sql1 = "INSERT INTO assignments (ASSIGN_NO, UNIT_NO, ASSIGN_MARK) VALUES (?, ?, ?)";
+				PreparedStatement statement1 = connection.prepareStatement(sql1);
+				
+				statement1.setInt(2, i + 100);
+
+				for (int j = 0; j < NumberofAssignments; j++) {
+					statement1.setInt(1, j);
+					statement1.setInt(3, AssignmentArray[j]);
+						
+					System.out.println("connection success!!");
+				
+					int rows1 = statement1.executeUpdate();
+					if (rows1 > 0) {
+						System.out.println("Assignment Update Success!!");
+					}
+				}
+				
+				statement1.close();
+				connection.close();
+				
+				statement.close();
+				connection.close();
+			} catch (SQLException e) {
+				System.out.println("& i oop");
+				e.printStackTrace();
+			}
+			}
  		int sum = IntStream.of(unitweighted).sum();
 		double WeightSum = Arrays.stream(Weights).sum();
 		if (WeightSum != 1.0) {
