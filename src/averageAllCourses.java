5ff20315d31ffe5b2937d9cce39f28c08a4c1e3a
Ada W <adabingw@yahoo.com>
2021-07-30 10:56:25 -0600

    Update averageAllCourses.java

--- src/averageAllCourses.java
+++ src/averageAllCourses.java
@@ -4,6 +4,10 @@ import javax.swing.JOptionPane;
 
 public class averageAllCourses {
 	public averageAllCourses() {
+		String url = "jdbc:mysql://localhost:3306/grades";
+		String username = "root";
+		String password = "/////";
+		
 		qForCourses C = new qForCourses();
 		int NCourses = C.AskCourses(); 
 		System.out.println("Number of Courses: " + NCourses);
@@ -14,10 +18,59 @@ public class averageAllCourses {
 			System.out.println("Course name: " + CNames[i]);
 			averageOneCourse AOC = new averageOneCourse(CNames[i]);
 			avg[i] = AOC.returnAverage();
-			System.out.println("The course mark for " + CNames[i] + " is " + avg[i]);
+			System.out.println("The course mark for " + CNames[i] + " is " + avg[i]);			
+			try {				
+				Connection connection = DriverManager.getConnection(url, username, password);
+				System.out.println("connection success!!");
+				
+				String sql = "UPDATE unit SET course_name = ? WHERE course_id = ?";
+				PreparedStatement statement = connection.prepareStatement(sql);
+				statement.setString(1, CNames[i]);
+				statement.setInt(2, i + 1);
+				
+				String sql1 = "UPDATE assignments SET course_name = ? WHERE course_id = ?";
+				PreparedStatement statement1 = connection.prepareStatement(sql1);
+				statement1.setString(1, CNames[i]);
+				statement1.setInt(2, i + 1);
+						
+				int rows = statement.executeUpdate();
+				if (rows > 0) {
+					System.out.println("COURSE_ID UNIT UPDATE SUCCESSFUL!");
+				}
+				
+				int rows1 = statement1.executeUpdate();
+				if (rows1 > 0) {
+					System.out.println("COURSE_ID ASSIGNMENTS UPDATE SUCCESSFUL!");
+				} 
+				
+				String sql2 = "INSERT INTO course (COURSE_NAME, COURSE_MARK) VALUES (?, ?)";
+				PreparedStatement statement2 = connection.prepareStatement(sql2);
+				statement2.setString(1, CNames[i]);
+				statement2.setInt(2, avg[i]);
+				
+				int rows2 = statement2.executeUpdate();
+				if (rows2 > 0) {
+					System.out.println("COURSE TABLE update successful!!");
+				}
+			
+				
+				statement.close();
+				connection.close();
+			
+				statement1.close();
+				connection.close();
+				
+				statement2.close();
+				connection.close();
+			} catch (SQLException e) {
+				System.out.println("& i oop");
+				e.printStackTrace();
+			}
 		}
+
 		int sumAverage = IntStream.of(avg).sum();
-		System.out.println("Total average is: " + sumAverage);
-		JOptionPane.showMessageDialog(null, "The final mark for this course is: " + sumAverage);
+		int totalAverage = sumAverage / NCourses;
+		System.out.println("Total average is: " + totalAverage);
+		JOptionPane.showMessageDialog(null, "The total average is: " + totalAverage);
 	}
 }
