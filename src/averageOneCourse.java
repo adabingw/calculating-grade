349ecd625ab5db1878b5f1d5a0b8e3309a0235e5
Ada W <adabingw@yahoo.com>
2021-08-06 14:34:40 -0600

    Update averageOneCourse.java

--- src/averageOneCourse.java
+++ src/averageOneCourse.java
@@ -10,11 +10,14 @@ import javax.swing.JOptionPane;
 public class averageOneCourse {
 	
 	int sum;
+	int UnitWeightLOL;
+	int assign;
+	String[] unitInfo = new String[2];
 	
 	public averageOneCourse(String courseName, boolean guest, String user_id, String name, boolean end, String pswrd) {
 		String url = "jdbc:mysql://localhost:3306/grades";
 		String username = "root";
-		String password = "/////";
+		String password = "Will@3229967163";
 	
 		qForUnits Q = new qForUnits();
 		int NUnits = Q.AskUnits(courseName); 
@@ -22,24 +25,62 @@ public class averageOneCourse {
 		int[] unitUNweighted = new int[NUnits];
 		int[] unitweighted = new int[NUnits]; 
 		int[] Weights = new int[NUnits];
-	
+		
 		for (int i = 0; i < NUnits; i++) {
-			int Weight = Q.Unitw(i+1, courseName); 
-			Weights[i] = Weight;
+			String[] unitInfo = Q.Unitw(i+1, courseName);
+		    try {
+		    	UnitWeightLOL = Integer.parseInt(unitInfo[0]);
+				System.out.println("Unit weight is " + UnitWeightLOL + "%"); // system check
+				if (UnitWeightLOL < 0 || UnitWeightLOL > 100) {
+		    	    JOptionPane.showMessageDialog(null, "Input out of bounds",
+		    	    	      "ERROR", JOptionPane.ERROR_MESSAGE);
+		    	    System.exit(0);
+				}
+	    	 } catch(NumberFormatException e) {
+	    	    JOptionPane.showMessageDialog(null, "Input is not a number",
+	    	    	      "ERROR", JOptionPane.ERROR_MESSAGE);
+	    	    System.exit(0);
+	    	 }
+		    
+			Weights[i] = UnitWeightLOL;
+			System.out.println("LALALA WEIGHTS[I] IS" + Weights[i]);
 			int NumberofAssignments = Q.AskAssign(i + 1, courseName); 
-			int[] AssignmentArray = Q.InputAssign(NumberofAssignments, i + 1, courseName);
-				int sumOfAssignments = IntStream.of(AssignmentArray).sum();
-				int averageAssignments = (int) Math.round((sumOfAssignments / NumberofAssignments) * 0.7);
+			int[] assigns = new int[NumberofAssignments];
+			String assignN;
+			String[] assignName = new String[NumberofAssignments];
+			String[][] AssignmentArray = Q.InputAssign(NumberofAssignments, i + 1, courseName);
+				
+		    try {
+		    	for(int j = 0; j < NumberofAssignments; j++) {
+			    	assign = Integer.parseInt(AssignmentArray[0][j]);
+			    	assignN = AssignmentArray[1][j];
+			    	assigns[j] = assign;
+			    	assignName[j] = assignN;
+		    	}
+				System.out.println("Unit weight is " + UnitWeightLOL + "%"); // system check
+				if (UnitWeightLOL < 0 || UnitWeightLOL > 100) {
+		    	    JOptionPane.showMessageDialog(null, "Input out of bounds",
+		    	    	      "ERROR", JOptionPane.ERROR_MESSAGE);
+		    	    System.exit(0);
+				}
+	    	 } catch(NumberFormatException e) {
+	    	    JOptionPane.showMessageDialog(null, "Input is not a number",
+	    	    	      "ERROR", JOptionPane.ERROR_MESSAGE);
+	    	    System.exit(0);
+	    	 }
+			
+			int sumOfAssignments = IntStream.of(assigns).sum();
+			int averageAssignments = (int) Math.round((sumOfAssignments / NumberofAssignments) * 0.7);
 			int InputedUFinal = Q.InputUnitFinal(i + 1, courseName);
 			int UnitFinaleW = (int) Math.round(InputedUFinal * 0.3);
 			unitUNweighted[i] = (int) Math.round(averageAssignments + UnitFinaleW);
-			unitweighted[i] = (int) Math.round(unitUNweighted[i] * ((double) Weight/100));
-		
+			unitweighted[i] = (int) Math.round(unitUNweighted[i] * ((double) Weights[i]/100));
+			
 			if (guest == false) {
 		
 				try {
 					Connection connection = DriverManager.getConnection(url, username, password);
-					String sql = "INSERT INTO unit (UNIT_ID, UNIT_W, UNIT_FINAL, UNIT_MARK, USER_ID, COURSE_NAME) VALUES (?, ?, ?, ?, ?, ?)";
+					String sql = "INSERT INTO unit (UNIT_ID, UNIT_W, UNIT_FINAL, UNIT_MARK, USER_ID, COURSE_NAME, UNIT_NAME) VALUES (?, ?, ?, ?, ?, ?, ?)";
 					PreparedStatement statement = connection.prepareStatement(sql);
 					statement.setInt(1, i + 100);
 					statement.setInt(2, Weights[i]);
@@ -47,6 +88,7 @@ public class averageOneCourse {
 					statement.setInt(4, unitUNweighted[i]);
 					statement.setString(5, user_id);
 					statement.setString(6, courseName);
+					statement.setString(7, unitInfo[1]);
 				
 					System.out.println("connection success!!");
 				
@@ -55,16 +97,18 @@ public class averageOneCourse {
 						System.out.println("A row has been inserted.");
 					}
 					
-					String sql1 = "INSERT INTO assignments (ASSIGN_ID, UNIT_ID, ASSIGN_MARK, USER_ID, COURSE_NAME) VALUES (?, ?, ?, ?, ?)";
+					String sql1 = "INSERT INTO assignments (ASSIGN_ID, UNIT_ID, ASSIGN_MARK, USER_ID, COURSE_NAME, UNIT_NAME, ASSIGN_NAME) VALUES (?, ?, ?, ?, ?, ?, ?)";
 					PreparedStatement statement1 = connection.prepareStatement(sql1);
 				
 					statement1.setInt(2, i + 100);
 					statement1.setString(4, user_id);
 					statement1.setString(5, courseName);
+					statement1.setString(6, unitInfo[1]);
 	
 					for (int j = 0; j < NumberofAssignments; j++) {
 						statement1.setInt(1, 1000 + j);
-						statement1.setInt(3, AssignmentArray[j]);
+						statement1.setInt(3, assigns[j]);
+						statement1.setString(7, assignName[j]);
 						
 						System.out.println("connection success!!");
 				
