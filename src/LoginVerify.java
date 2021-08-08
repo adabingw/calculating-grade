e857250861a3715c887c37f67ed6f2168559c51a
Ada W <adabingw@yahoo.com>
2021-08-08 17:39:51 -0600

    Update LoginVerify.java

--- src/LoginVerify.java
+++ src/LoginVerify.java
@@ -13,28 +13,110 @@ import javax.swing.SwingConstants;
 
 public class LoginVerify {
 	
+   protected JOptionPane getOptionPane(JComponent parent) {
+        JOptionPane pane = null;
+        if (!(parent instanceof JOptionPane)) {
+            pane = getOptionPane((JComponent)parent.getParent());
+        } else {
+            pane = (JOptionPane) parent;
+        }
+        return pane;
+    }
+	
 	public LoginVerify() {
 		
 		String url = "jdbc:mysql://localhost:3306/grades";
 		String username = "root";
 		String password = " ";
 		
-        JPasswordField jpf = new JPasswordField(10);
- 	    JLabel jl = new JLabel("Password: ");
+        	JPasswordField jpf = new JPasswordField(10);
+ 	    	JLabel jl = new JLabel("Password: ");
 		jl.setFont(new Font("Times New Roman", Font.PLAIN, 15));
  	    
 		JTextField username1 = new JTextField(10);
-	    JLabel label = new JLabel("Username: ");
+	    	JLabel label = new JLabel("Username: ");
 		label.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		
+		        final JButton okay = new JButton("Ok");
+        okay.setBackground(Color.WHITE);
+        okay.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+        okay.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane pane = getOptionPane((JComponent)e.getSource());
+                pane.setValue(okay);
+            }
+        });
+        okay.setEnabled(false);
+        
+        final JButton cancel = new JButton("Cancel");
+        cancel.setBackground(Color.WHITE);
+        cancel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane pane = getOptionPane((JComponent)e.getSource());
+                pane.setValue(cancel);
+            }
+        });
+        
+        username1.getDocument().addDocumentListener(new DocumentListener() {
+            protected void update() {
+                okay.setEnabled(username1.getText().length() > 0 && jpf.getPassword().length > 0);
+            }
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                update();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                update();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                update();
+            }
+        });
+        
+        jpf.getDocument().addDocumentListener(new DocumentListener() {
+            protected void update() {
+                okay.setEnabled(username1.getText().length() > 0 && jpf.getPassword().length > 0);
+            }
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                update();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                update();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                update();
+            }
+        });
 		
 		Object[] message = {
 				label, username1,
 				jl, jpf,
 		};
 	    
-	    int result = JOptionPane.showConfirmDialog(null, message, 
-	             "LOGIN", JOptionPane.OK_CANCEL_OPTION);
-	    if (result == JOptionPane.OK_OPTION) {
+        int value = JOptionPane.showOptionDialog(
+       	     null,
+       	     message,
+       	     "LOGIN",
+       	     JOptionPane.YES_NO_OPTION, 
+       	     JOptionPane.QUESTION_MESSAGE, 
+       	     null, 
+       	     new Object[]{okay, cancel}, 
+       	     okay);
+	    if (value == 0) {
 	       String user = username1.getText();
 	       char[] p = jpf.getPassword();
 	       String pswrd = String.valueOf(p);
@@ -70,9 +152,9 @@ public class LoginVerify {
 				System.out.println("& i oop");
 				e.printStackTrace();
 	        }	   
-	    } else {
-	    	   System.exit(0);
-	       }
+	    } else if (value == 1) {
+        	JOptionPane.getRootFrame().dispose();   
+        } else System.exit(0);
 	    
 	    return; 
     }
