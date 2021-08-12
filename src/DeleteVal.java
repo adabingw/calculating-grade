d23cb0243327f1979be15cc7db1112100b6c5668
Ada W <adabingw@yahoo.com>
2021-08-11 20:25:17 -0600

    Update DeleteVal.java

--- src/DeleteVal.java
+++ src/DeleteVal.java
@@ -13,7 +13,7 @@ public class DeleteVal {
 	
 	String url = "jdbc:mysql://localhost:3306/grades";
 	String username = "root";
-	String password = "root";
+	String password = " ";
 	
 	public DeleteVal(int c, Object o, Object o1, Object o2, int tableNo, JFrame f, String user_id, String name, String pswrd) {
 		JLabel label = new JLabel("Are you sure?");
@@ -34,8 +34,20 @@ public class DeleteVal {
 		    	   statement.setInt(1, i);
 		    	   
 					int rows = statement.executeUpdate();
+					
+		    	   String sql1 = "DELETE FROM unit WHERE course_id = ?";
+		    	   PreparedStatement statement1 = connection.prepareStatement(sql1);
+		    	   statement1.setInt(1, i);
+		    	   
+					int rows1 = statement1.executeUpdate();
+						
+		    	   String sql2 = "DELETE FROM assignments WHERE course_id = ?";
+		    	   PreparedStatement statement2 = connection.prepareStatement(sql2);
+		    	   statement2.setInt(1, i);
+		    	   
+					int rows2 = statement2.executeUpdate();
 	
-					if (rows > 0) {
+					if (rows > 0 || rows1 > 0 || rows2 > 0) {
 						f.setVisible(false);
 						new ViewDatabase(user_id, name, pswrd);
 						System.out.println("DELETEVAL");
@@ -45,42 +57,52 @@ public class DeleteVal {
             	   int i = Integer.parseInt((String) o );
             	   int x = Integer.parseInt((String) o1 );
 
-		   String sql = "DELETE FROM unit WHERE unit_id = ? && course_id = ?";
-		   PreparedStatement statement = connection.prepareStatement(sql);
-		   statement.setInt(1, x);
-		   statement.setInt(2, i);
+		    	   String sql = "DELETE FROM unit WHERE unit_id = ? && course_id = ?";
+		    	   PreparedStatement statement = connection.prepareStatement(sql);
+		    	   statement.setInt(1, x);
+		    	   statement.setInt(2, i);
 		    	   
-		   int rows = statement.executeUpdate();
-
-		   if (rows > 0) {
-			f.setVisible(false);
-			new ViewDatabase(user_id, name, pswrd);
-			System.out.println("DELETEVAL");
-		   }
+		    	   String sql1 = "DELETE FROM assignments WHERE unit_id = ? && course_id = ?";
+		    	   PreparedStatement statement1 = connection.prepareStatement(sql1);
+		    	   statement1.setInt(1, x);
+		    	   statement1.setInt(2, i);
+		    	   
+					int rows = statement.executeUpdate();
+					int rows1 = statement1.executeUpdate();
+	
+					if (rows > 0) {
+						f.setVisible(false);
+						new ViewDatabase(user_id, name, pswrd);
+						System.out.println("DELETEVAL");
+					}
+					
+					if (rows1 > 0) {
+						System.out.println("DELETEVAL2");
+					}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                } else if (tableNo == 3) {
       	    	   int i = Integer.parseInt((String) o );
     	    	   int x = Integer.parseInt((String) o1 );
     	    	   int j = Integer.parseInt((String) o2 );
-		   String sql = "DELETE FROM assignments WHERE course_id = ? && unit_id = ? && assign_id = ?";
-		   PreparedStatement statement = connection.prepareStatement(sql);
-		   statement.setInt(1, i);
-		   statement.setInt(2, x);
-		   statement.setInt(3, j);
-
-		   int rows = statement.executeUpdate();
-
-		   if (rows > 0) {
-			f.setVisible(false);
-			new ViewDatabase(user_id, name, pswrd);
-			System.out.println("DELETEVAL");
-		   }
+		    	   String sql = "DELETE FROM assignments WHERE course_id = ? && unit_id = ? && assign_id = ?";
+		    	   PreparedStatement statement = connection.prepareStatement(sql);
+		    	   statement.setInt(1, i);
+		    	   statement.setInt(2, x);
+		    	   statement.setInt(3, j);
+		    	   
+					int rows = statement.executeUpdate();
+	
+					if (rows > 0) {
+						f.setVisible(false);
+						new ViewDatabase(user_id, name, pswrd);
+						System.out.println("DELETEVAL");
+					}
                }
                
-		} catch (SQLException e) {
+		 } catch (SQLException e) {
 			System.out.println("& i oop");
 			e.printStackTrace();
-		}
+	        }
 			
 		}
 	}
