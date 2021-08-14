fa34c220d17b4222b19d3d9001b0338d811ea756
Ada W <adabingw@yahoo.com>
2021-08-14 12:13:41 -0600

    Update and rename averageAllCourses.java to AverageAllCourses.java

--- src/AverageAllCourses.java
+++ src/AverageAllCourses.java
@@ -1,24 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.stream.IntStream;
 
 import javax.swing.JOptionPane;
 
-public class averageAllCourses {
-	public averageAllCourses(boolean guest, String user_id, String name, String pswrd) {
+public class AverageAllCourses {
+	public AverageAllCourses(boolean guest, String user_id, String name, String pswrd) {
+		
 		String url = "jdbc:mysql://localhost:3306/grades";
 		String username = "root";
-		String password = "/////";
+		String password = "root";
 		
-		qForCourses C = new qForCourses();
-		int NCourses = C.AskCourses(); 
-		System.out.println("Number of Courses: " + NCourses);
-		String[] CNames = new String[NCourses];
-		int[] avg = new int[NCourses];
-		for (int i = 0; i < NCourses; i++) {
-			CNames[i] = C.CourseName(i + 1);
-			System.out.println("Course name: " + CNames[i]);
-			averageOneCourse AOC = new averageOneCourse(CNames[i], guest, user_id, name, false, pswrd);
-			avg[i] = AOC.returnAverage();
-			System.out.println("The course mark for " + CNames[i] + " is " + avg[i]);
+		QForCourses c = new QForCourses();
+		int nCourses = c.askCourses();
+		System.out.println("Number of Courses: " + nCourses);
+		String[] cNames = new String[nCourses];
+		int[] avg = new int[nCourses];
+		
+		for (int i = 0; i < nCourses; i++) {
+			cNames[i] = c.courseName(i + 1);
+			System.out.println("Course name: " + cNames[i]);
+			AverageOneCourse aoc = new AverageOneCourse(cNames[i], guest, user_id, name, false, pswrd);
+			avg[i] = aoc.returnAverage();
+			System.out.println("The course mark for " + cNames[i] + " is " + avg[i]);
 			
 			if (guest == false) {
 			
@@ -29,12 +35,12 @@ public class averageAllCourses {
 					String sql = "UPDATE unit SET course_id = ? WHERE course_name = ?";
 					PreparedStatement statement = connection.prepareStatement(sql);
 					statement.setInt(1, i+1);
-					statement.setString(2, CNames[i]);
-					
+					statement.setString(2, cNames[i]);
+					//
 					String sql1 = "UPDATE assignments SET course_id = ? WHERE course_name = ?";
 					PreparedStatement statement1 = connection.prepareStatement(sql1);
 					statement1.setInt(1, i+1);
-					statement1.setString(2, CNames[i]);
+					statement1.setString(2, cNames[i]);
 							
 					int rows = statement.executeUpdate();
 					if (rows > 0) {
@@ -48,7 +54,7 @@ public class averageAllCourses {
 					
 					String sql2 = "INSERT INTO course (COURSE_NAME, COURSE_MARK, USER_ID, COURSE_ID) VALUES (?, ?, ?, ?)";
 					PreparedStatement statement2 = connection.prepareStatement(sql2);
-					statement2.setString(1, CNames[i]);
+					statement2.setString(1, cNames[i]);
 					statement2.setInt(2, avg[i]);
 					statement2.setString(3, user_id);
 					statement2.setInt(4, i+1);
@@ -74,12 +80,12 @@ public class averageAllCourses {
 		}}
 
 		int sumAverage = IntStream.of(avg).sum();
-		int totalAverage = sumAverage / NCourses;
+		int totalAverage = sumAverage / nCourses;
 		System.out.println("Total average is: " + totalAverage);
 		JOptionPane.showMessageDialog(null, "The total average is: " + totalAverage);
 		
 		if (guest == false) {
-			new userMain(user_id, name, pswrd);
+			new UserMain(user_id, name, pswrd, null);
 		}
 	}
 }
